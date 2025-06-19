7df71ee654290b4a0077a783d04150dea4a1ce41
satwik <mohanty.satwik290@gmail.com>
2025-06-19 22:56:12 +0530

    datatypes in ts

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,15 @@
 "use strict";
-const a = 123;
+// let a =25;
+// // type error
+// // a="a";
+// let nambo="satwik"; //string
+let a;
+let nambo;
+let check;
+let named = 34;
+let surname; //union
+// func
+const func = (n, m) => {
+    console.log(n, m);
+    return n * m;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-const a= 123;
+// let a =25;
+// // type error
+// // a="a";
+// let nambo="satwik"; //string
+
+let a:string;
+let nambo:number;
+let check:boolean;
+let named=34;
+
+let surname:string | number; //union
+// func
+const func=(n:number,m:number)=>{
+   console.log(n,m);
+   return n*m;
+};
+
 
  
