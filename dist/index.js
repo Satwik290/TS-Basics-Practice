0a8a68c8a3cec53d08556b5c77405b736f10e0c2
satwik <mohanty.satwik290@gmail.com>
2025-06-22 12:56:08 +0530

    objects in TS

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,30 @@
 "use strict";
-const arr = [1, 2, 3,];
-const arr2 = new Array(20);
-const arr3 = [158, "123", 123];
-arr2[0] = "satwik";
-arr.forEach(i => {
-    i.valueOf();
-});
+;
+;
+const gg = {
+    height: 12,
+    weight: 34,
+    scholar: true,
+    func: (n, m) => {
+        console.log(n * m);
+    },
+};
+const ken = {
+    height: 12,
+    weight: 34,
+    scholar: true,
+    func: (n, m) => {
+        console.log(n * m);
+    },
+};
+ken.func(12, 54);
+const obj = {
+    height: 52,
+    weight: 45,
+    gender: true,
+};
+const obj2 = {
+    height: 52,
+    weight: 45,
+    // gender: 
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,47 @@
-const arr:number[] = [1,2,3,];
-const arr2:Array<string>= new Array(20);
-const arr3:Array<string | number>= [158,"123",123];
-arr2[0]="satwik"; 
+interface Obj{
+   height: number;
+   weight: number;
+   gender?: boolean;
+};
+// interface act as class 
 
-arr.forEach(i=>{
-   i.valueOf();
-});
+//type
+type typefunc = (n:number,m:number)=> void;
+
+interface newObj extends Obj {
+ scholar:boolean;
+ func: typefunc;
+};
+
+const gg: newObj={
+  height:12,
+  weight:34,
+  scholar:true,
+  func: (n,m)=>{
+   console.log(n*m);
+  },
+};
+
+
+const ken: newObj={
+  height:12,
+  weight:34,
+  scholar:true,
+  func: (n,m)=>{
+   console.log(n*m);
+  },
+}; 
+
+ken.func(12,54);
+
+const  obj: Obj={ 
+   height: 52,
+   weight: 45,
+   gender: true,
+};
+
+const  obj2: Obj={ 
+   height: 52,
+   weight: 45,
+   // gender: 
+};
