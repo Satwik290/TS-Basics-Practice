30a02465827d783df08b5aabf5e7b748d72c4a7d
satwik <mohanty.satwik290@gmail.com>
2025-06-25 16:48:28 +0530

    functions in ts

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,76 @@
-interface Obj{
-   height: number;
-   weight: number;
-   gender?: boolean;
-};
-// interface act as class 
+// interface Obj{
+//    height: number;
+//    weight: number;
+//    gender?: boolean;
+// };
+// // interface act as class 
 
-//type
-type typefunc = (n:number,m:number)=> void;
+// //type
+// type typefunc = (n:number,m:number)=> void;
 
-interface newObj extends Obj {
- scholar:boolean;
- func: typefunc;
-};
+// interface newObj extends Obj {
+//  scholar:boolean;
+//  func: typefunc;
+// };
 
-const gg: newObj={
-  height:12,
-  weight:34,
-  scholar:true,
-  func: (n,m)=>{
-   console.log(n*m);
-  },
-};
+// const gg: newObj={
+//   height:12,
+//   weight:34,
+//   scholar:true,
+//   func: (n,m)=>{
+//    console.log(n*m);
+//   },
+// };
 
 
-const ken: newObj={
-  height:12,
-  weight:34,
-  scholar:true,
-  func: (n,m)=>{
-   console.log(n*m);
-  },
-}; 
+// const ken: newObj={
+//   height:12,
+//   weight:34,
+//   scholar:true,
+//   func: (n,m)=>{
+//    console.log(n*m);
+//   },
+// }; 
 
-ken.func(12,54);
+// ken.func(12,54);
 
-const  obj: Obj={ 
-   height: 52,
-   weight: 45,
-   gender: true,
-};
+// const  obj: Obj={ 
+//    height: 52,
+//    weight: 45,
+//    gender: true,
+// };
 
-const  obj2: Obj={ 
-   height: 52,
-   weight: 45,
-   // gender: 
-};
+// const  obj2: Obj={ 
+//    height: 52,
+//    weight: 45,
+//    // gender: 
+// };
+
+/*
+type Functype = (n:number,m:number,l?:number)=> number|string;
+const func:Functype =(n,m,l)=>{
+   if(typeof l === "undefined")return n*m;
+   return n*m*l;
+}
+func(1,3) */
+
+//default
+/*
+type Functype = (n:number,m:number,l?:number)=> number|string;
+const func:Functype =(n,m,l=7)=>{
+   if(typeof l === "undefined")return n*m;
+   return n*m*l;
+}
+func(1,3) ; */
+
+//rest operator
+/** 
+type Functype = (...m:number[])=> number[];
+const func:Functype=(...m)=>{
+   return m;
+}
+func(1,3,9,65,97) */
+
+// function lol(n:number):number{
+//    return 54;
+// }
